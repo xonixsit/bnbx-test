7be9d5e0e9118307eca7e5c4452b0de513f04142
xonixsit <xonixsitsolutions@gmail.com>
2025-04-11 16:08:09 -0700

    theme update

--- Prod-Server/development0104/admin/src/environments/environment.staging.ts
+++ Prod-Server/development0104/admin/src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    //apiUrl: 'https://bnbx.pro/api', // Live API URL
-    apiUrl: 'http://localhost:8080', // Local API URL
+    apiUrl: 'https://bnbx.pro/api', // Live API URL
+    //apiUrl: 'http://localhost:8080', // Local API URL
 };
